2ced87df802af2f4749196dc7dc42370ce97529e
Ko Tashiro <tai1fuu7@gmail.com>
2020-03-14 08:41:36 +0900

    ユーザ登録機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true
+
+  validates :name, presence: true
+  validates :email, presence: true
+
+  validates :password_confirmation, presence: true
+
+  has_secure_password
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # get 'pages/index'
-  get "pages/index" => "pages#index"
 
-  # get 'home/top'
-  get "/" => "home#top"
+  get '/', to: 'pages#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
